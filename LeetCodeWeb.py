aa321afbb9f78cb14450cebe23228271141ca0db
Tong ZHAN <zhantong1994@163.com>
2017-08-05 20:18:40 +0800

    可以在网页中显示特定题目的代码，并高亮

--- LeetCodeWeb.py
+++ LeetCodeWeb.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 import json
 import os.path
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
 
 app = Flask(__name__)
 ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -19,7 +22,8 @@ def show_problem(slug):
     description_file_name = str(info['id']).zfill(3) + '. ' + info['title'] + '.html'
     with open(os.path.join(ROOT, 'descriptions', description_file_name), 'r', encoding='utf-8') as f:
         description = f.read()
-    return render_template('problem.html', description=description)
+    codes = get_codes(('python', 'java', 'c++'), info)
+    return render_template('problem.html', description=description, codes=codes)
 
 
 def init():
@@ -30,6 +34,22 @@ def init():
             'id': item['stat']['question_id'],
             'title': item['stat']['question__title']
         }
+
+
+def get_codes(code_types, info):
+    code_infos = {
+        'java': ('Java', 'java'),
+        'python': ('Python', 'py'),
+        'c++': ('C++', 'cpp')
+    }
+    codes = []
+    for code_type in code_types:
+        code_info = code_infos[code_type]
+        with open(os.path.join(ROOT, 'leetcode', str(info['id']).zfill(3) + '. ' + info['title'], code_info[0],
+                               info['title'] + '.' + code_info[1]), 'r', encoding='utf-8') as f:
+            python_code = highlight(f.read(), PythonLexer(), HtmlFormatter())
+            codes.append(python_code)
+    return codes
 
 
 init()
