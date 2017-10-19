f92cf57d773b8eb0e4e338e300221c5203b9e38f
Tong ZHAN <zhantong1994@163.com>
2017-10-19 09:54:20 +0800

    用SQLite替换JSON，更新文件结构

--- LeetCodeWeb.py
+++ LeetCodeWeb.py
@@ -2,15 +2,24 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
-import json
+from flask import g
 import os.path
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
+import sqlite3
 
 app = Flask(__name__)
 ROOT = os.path.realpath(os.path.dirname(__file__))
-slug_dict = {}
+
+DATABASE = 'leetcode.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 
 @app.route('/')
@@ -20,40 +29,39 @@ def hello_world():
 
 @app.route('/problems')
 def show_problem_list():
-    problem_list = get_problem_list(slug_dict)
+    problem_list = get_problem_list()
     return render_template('problems_summary.html', problem_list=problem_list)
 
 
 @app.route('/problems/<slug>')
 def show_problem(slug):
-    info = slug_dict[slug]
-    description_file_name = str(info['id']).zfill(3) + '. ' + info['title'] + '.html'
+    c = get_db().cursor()
+    c.execute('SELECT id,title FROM problem WHERE slug=?', (slug,))
+    id, title = c.fetchone()
+    description_file_name = str(id).zfill(3) + '. ' + title + '.html'
     file_path = os.path.join(ROOT, 'descriptions', description_file_name)
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             description = f.read()
     else:
         description = '收费题目'
-    codes = get_codes(('python', 'java', 'c++'), info)
-    title = str(info['id']) + '. ' + info['title']
+    codes = get_codes(('python', 'java', 'c++'), id, title)
+    title = str(id) + '. ' + title
     if 'X-PJAX' in request.headers:
         return render_template('problem_description.html', description=description, codes=codes, title=title,
-                               id=info['id'])
+                               id=id)
     return render_template('problem.html', description=description, codes=codes,
-                           problem_list=get_problem_list(slug_dict), title=title, id=info['id'])
+                           problem_list=get_problem_list(), title=title, id=id)
 
 
-def init():
-    with open(os.path.join(ROOT, 'problem_list.json'), 'r', encoding='utf-8') as f:
-        content = json.loads(f.read())
-    for item in content:
-        slug_dict[item['stat']['question__title_slug']] = {
-            'id': item['stat']['question_id'],
-            'title': item['stat']['question__title']
-        }
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
-def get_codes(code_types, info):
+def get_codes(code_types, id, title):
     code_infos = {
         'java': ('Java', 'java'),
         'python': ('Python', 'py'),
@@ -62,8 +70,8 @@ def get_codes(code_types, info):
     codes = []
     for code_type in code_types:
         code_info = code_infos[code_type]
-        file_path = os.path.join(ROOT, 'leetcode', str(info['id']).zfill(3) + '. ' + info['title'], code_info[0],
-                                 info['title'] + '.' + code_info[1])
+        file_path = os.path.join(ROOT, 'submissions', str(id).zfill(3) + '. ' + title, code_info[0],
+                                 'Solution.' + code_info[1])
         if not os.path.exists(file_path):
             continue
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -72,18 +80,17 @@ def get_codes(code_types, info):
     return codes
 
 
-def get_problem_list(slug_dict):
+def get_problem_list():
     problem_list = []
-    for key, value in slug_dict.items():
+    c = get_db().cursor()
+    for id, title, slug in c.execute('SELECT id,title,slug FROM problem ORDER BY id'):
         problem_list.append({
-            'id': value['id'],
-            'url': '/problems/' + key,
-            'name': str(value['id']).zfill(3) + '. ' + value['title']
+            'id': id,
+            'url': '/problems/' + slug,
+            'name': str(id).zfill(3) + '. ' + title
         })
-    problem_list.sort(key=lambda x: x['id'])
     return problem_list
 
 
-init()
 if __name__ == '__main__':
     app.run()
